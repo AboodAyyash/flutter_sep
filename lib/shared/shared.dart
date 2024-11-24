2c02db1639749a5bdc52fd106bc8c86b6ceea60d
AboodAyyash <46052083+AboodAyyash@users.noreply.github.com>
2024-11-24 20:31:55 +0300

    24-11-2024

---
+++ lib/shared/shared.dart
@@ -0,0 +1,23 @@
+List<Map> usersData = [
+  {
+    'age': "1",
+    'id': "1",
+    'userName': "1",
+    "email": "email1@email.com",
+    'password': "Password@123"
+  },
+  {
+    'age': "2",
+    'id': "2",
+    'userName': "2",
+    "email": "email2@email.com",
+    'password': "Password@123"
+  },
+  {
+    'age': "3",
+    'id': "3",
+    'userName': "3",
+    "email": "email3@email.com",
+    'password': "Password@123"
+  },
+];
